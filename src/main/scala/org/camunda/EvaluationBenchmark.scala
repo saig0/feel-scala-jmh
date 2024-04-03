6b673f558705c2a8b66a77313e453794ae6ee894
Philipp Ossler <philipp.ossler@gmail.com>
2024-04-03 09:01:15 +0200

    feat: Add evaluation benchmark

---
+++ src/main/scala/org/camunda/EvaluationBenchmark.scala
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2005, 2014, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package org.camunda
+
+import org.camunda.EvaluationBenchmark._
+import org.camunda.feel.FeelEngine
+import org.camunda.feel.syntaxtree.ParsedExpression
+import org.openjdk.jmh.annotations._
+
+import java.time.LocalDateTime
+import java.util.concurrent.TimeUnit
+
+@BenchmarkMode(Array(Mode.Throughput))
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@Warmup(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 10, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(1)
+class EvaluationBenchmark {
+
+  @Benchmark
+  def comparisonNumber(): Any = {
+    engine.eval(
+      exp = expressionComparisonNumber,
+      variables = Map("x" -> 21)
+    )
+  }
+
+  @Benchmark
+  def intervalNumbers(): Any = {
+    engine.eval(
+      exp = expressionIntervalNumbers,
+      variables = Map("x" -> 21)
+    )
+  }
+
+  @Benchmark
+  def intervalDateAndTime(): Any = {
+    engine.eval(
+      exp = expressionIntervalDateAndTime,
+      variables = Map("x" -> LocalDateTime.parse("2021-04-01T00:00:00"))
+    )
+  }
+
+  @Benchmark
+  def pathExpression(): Any = {
+    engine.eval(
+      exp = expressionPath,
+      variables = Map("order" ->
+        Map("tracking" ->
+          Map("id" -> "order-123")
+        )
+      )
+    )
+  }
+
+  @Benchmark
+  def filterExpression(): Any = {
+    engine.eval(
+      exp = expressionFilter,
+      variables = Map("order" ->
+        List(
+          Map("type" -> "priority")
+        )
+      )
+    )
+  }
+
+  @Benchmark
+  def listIndexAccess(): Any = {
+    engine.eval(
+      exp = expressionListIndexAccess,
+      variables = Map[String, Any]()
+    )
+  }
+
+  @Benchmark
+  def contextProjection(): Any = {
+    engine.eval(
+      exp = expressionContextProjection,
+      variables = Map[String, Any]()
+    )
+  }
+
+  @Benchmark
+  def forExpression(): Any = {
+    engine.eval(
+      exp = expressionFor,
+      variables = Map("orders" ->
+        List(
+          Map("id" -> "order-123")
+        )
+      )
+    )
+  }
+
+  @Benchmark
+  def contextLiteral(): Any = {
+    engine.eval(
+      exp = expressionContextLiteral,
+      variables = Map[String, Any]()
+    )
+  }
+
+  @Benchmark
+  def listLiteral(): Any = {
+    engine.eval(
+      exp = expressionListLiteral,
+      variables = Map[String, Any]()
+    )
+  }
+
+  @Benchmark
+  def functionInvocation(): Any = {
+    engine.eval(
+      exp = expressionFunctionInvocation,
+      variables = Map("x" -> "Hello camunda")
+    )
+  }
+
+}
+
+object EvaluationBenchmark {
+
+  @State(Scope.Benchmark)
+  private val engine = new FeelEngine()
+
+  @State(Scope.Benchmark)
+  private val expressionComparisonNumber = parseExpression("x = 21")
+
+  @State(Scope.Benchmark)
+  private val expressionIntervalNumbers = parseExpression("x between 10 and 30")
+
+  @State(Scope.Benchmark)
+  private val expressionIntervalDateAndTime = parseExpression("""x between date and time("2021-01-01T00:00:00") and date and time("2021-06-01T00:00:00")""")
+
+  @State(Scope.Benchmark)
+  private val expressionPath = parseExpression("order.tracking.id")
+
+  @State(Scope.Benchmark)
+  private val expressionFilter = parseExpression("""order[type = "priority"]""")
+
+  @State(Scope.Benchmark)
+  private val expressionListIndexAccess = parseExpression("[1,2,3][2]")
+
+  @State(Scope.Benchmark)
+  private val expressionContextProjection = parseExpression("{x:1,y:2,z:3}.x")
+
+  @State(Scope.Benchmark)
+  private val expressionFor = parseExpression("for order in orders return order.id")
+
+  @State(Scope.Benchmark)
+  private val expressionContextLiteral = parseExpression("{x:1,y:2,z:3}")
+
+  @State(Scope.Benchmark)
+  private val expressionListLiteral = parseExpression("[1,2,3]")
+
+  @State(Scope.Benchmark)
+  private val expressionFunctionInvocation = parseExpression("""contains(x, "camunda")""")
+
+  private def parseExpression(expression: String): ParsedExpression = engine.parseExpression(expression) match {
+    case Right(result) => result
+    case Left(failure) => throw new RuntimeException(s"Failed to parse expression: ${failure.message}")
+  }
+
+}
